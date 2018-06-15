2d2ae3c160b83917e1cce526f2af1c51e64f2be6
Luca Cadamuro <lc.cadamuro@gmail.com>
2018-06-15 09:02:22 -0500

    bb guns

---
+++ Configuration/python/bbgun_BjetToMu_endcap_pos_pythia8_cfi.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+
+generator = cms.EDFilter("Pythia8PtGun",
+                         PGunParameters = cms.PSet(
+        MaxPt = cms.double(500.0),
+        MinPt = cms.double(15.0),
+        ParticleID = cms.vint32(5),
+        AddAntiParticle = cms.bool(True),
+        MaxEta = cms.double(2.7),
+        MaxPhi = cms.double(3.14159265359),
+        MinEta = cms.double(1.0),
+        MinPhi = cms.double(-3.14159265359) ## in radians
+        ),
+                         Verbosity = cms.untracked.int32(0), ## set to 1 (or greater)  for printouts
+                         psethack = cms.string('bb gun pt 15 500'),
+                         firstRun = cms.untracked.uint32(1),
+                         PythiaParameters = cms.PSet(parameterSets = cms.vstring())
+                         )
+
+MuMuFilter = cms.EDFilter("MCParticlePairFilter",
+                          Status = cms.untracked.vint32(1, 1),
+                          MinPt = cms.untracked.vdouble(2., 2.),
+                          MinP  = cms.untracked.vdouble(0., 0.),
+                          MinEta = cms.untracked.vdouble(1.2, 1.2),
+                          MaxEta = cms.untracked.vdouble(2.5, 2.5),
+                          ParticleCharge = cms.untracked.int32(0),
+                          ParticleID1 = cms.untracked.vint32(13),
+                          )
+
+ProductionFilterSequence = cms.Sequence(generator*MuMuFilter)
